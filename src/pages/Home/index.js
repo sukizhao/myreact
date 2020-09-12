6d125ad92f228da59089eb29b0677eb7b0e70a67
zhaoshuqi <18518664759@163.com>
2020-09-12 20:59:34 +0800

    初始化

---
+++ src/pages/Home/index.js
@@ -0,0 +1,20 @@
+// Home\index.js
+
+/**
+ * @name Home
+ * @desc 首页
+ */
+
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
