6000a579fde477989a75c43b7d218daac3188ee1
zhaoshuqi <18518664759@163.com>
2020-09-13 10:24:48 +0800

    删除无效代码

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Routes from './router'
+import Routes from './router/index.js'
 
 class App extends Component {
     render() {
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from '../pages/Home'
 
 class Router extends Component {
     render() {
